ce0e484d529631a010e999964cd7d4eeaf88a9d1
Shweta Singhal <shweta.singhal@tavant.com>
2018-05-18 12:29:38 +0530

    Changes for Defect #2

--- GitTest/src/collectionExamples/LinklistAdd.java
+++ GitTest/src/collectionExamples/LinklistAdd.java
@@ -1,5 +1,21 @@
 package collectionExamples;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class LinklistAdd {
+	public static void main(String args[])
+	{
+		LinkedList list = new LinkedList();
+		list.add(10);
+		list.addFirst(20);
+		list.addLast(2);
+		list.add(4);
+		Iterator itr = list.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+	}
 
 }
