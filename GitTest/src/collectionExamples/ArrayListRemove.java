37b3ea6bf0efebfc4e6beb385c2b1f1724a97338
Shweta Singhal <shweta.singhal@tavant.com>
2018-05-18 11:19:25 +0530

    Create Project

---
+++ GitTest/src/collectionExamples/ArrayListRemove.java
@@ -0,0 +1,19 @@
+package collectionExamples;
+
+import java.util.ArrayList;
+
+public class ArrayListRemove {
+	public static void main(String args[])
+	{
+		ArrayList list = new ArrayList();
+		int[] arr = {1,4,3,24,5,65};
+		for(int i=0; i<arr.length; i++)
+		{
+		list.add(arr[i]);
+		}
+		System.out.println(list);
+		
+		
+	}
+
+}
---
+++ GitTest/src/collectionExamples/ArrayListTest.java
@@ -0,0 +1,24 @@
+package collectionExamples;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListTest {
+	public static void main(String args[])
+	{
+		ArrayList list = new ArrayList();
+		list.add(20);
+		list.add(23);
+		System.out.println(list.get(1));
+		System.out.println(list);
+		
+		Iterator itr = list.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+			//list.add(32);
+			System.out.println(itr.hashCode());
+		}
+	}
+
+}
